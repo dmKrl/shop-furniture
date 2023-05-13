7179f41675a92c23dc16ef70834c5ee850a1cf1b
Dmitry Kralichkin <dm_krl@mail.ru>
2023-05-13 16:04:00 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+// const cartDelete = document.querySelector('.cart_delete');
+// const cartBlock = document.querySelector('.cart__item_second');
+
+// // cartDelete.addEventListener("click", () => {
+// //     cartBlock.classList.add('hidden');
+// // })
+
+// cartDelete.addEventListener("click", () => {
+//     if ((confirm('Удалить товар?')) === true) {
+//         cartBlock.style.display = "none";
+//     } else {
+//         return false;
+//     }    
+// })
+
+
+
+// // cartDelete.onclick = () => {
+// //     console.log('111');
+// // }
